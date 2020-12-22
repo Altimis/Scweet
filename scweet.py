58eeb437edd8f5a4834de70a0226bf2796163de3
Yassine Ait Jeddi <48790841+Altimis@users.noreply.github.com>
2020-12-22 16:32:51 +0100

    proxy_settings

--- scweet.py
+++ scweet.py
@@ -124,7 +124,7 @@ def log_search_page(driver, start_date, end_date, lang, display_type, words, to_
         print("Latest Button doesnt exist.")
     
 
-def init_driver(navig, headless):
+def init_driver(navig, headless,proxy):
     # create instance of web driver
     #path to the chromdrive.exe
     if navig == "chrome":
@@ -136,6 +136,8 @@ def init_driver(navig, headless):
         	options.headless=False
         options.add_argument('--disable-gpu')
         options.add_argument('log-level=3')
+        if proxy!=None:
+        	options.add_argument('--proxy-server=%s' % proxy)
         prefs = {"profile.managed_default_content_settings.images": 2}
         options.add_experimental_option("prefs", prefs)
         driver = webdriver.Chrome(options=options,executable_path=browser_path)
@@ -144,6 +146,8 @@ def init_driver(navig, headless):
     elif navig == "edge":
         browser_path = './drivers/msedgedriver.exe'
         options = EdgeOptions()
+        if proxy!=None:
+        	options.add_argument('--proxy-server=%s' % proxy)
         if headless==True:
         	options.headless = True
         	options.use_chromium = False
@@ -205,7 +209,7 @@ def keep_scroling(driver, data, writer, tweet_ids, scrolling, tweet_parsed, limi
 				break
 	return driver,data,writer, tweet_ids,scrolling, tweet_parsed, scroll, last_position
 
-def scrap(start_date, max_date, words,to_accounts, from_accounts, days_between=5, navig="chrome", lang="en", headless=True, limit_search=1000, display_type="Top", resume=False):
+def scrap(start_date, max_date, words,to_accounts, from_accounts, days_between=5, navig="chrome", lang="en", headless=True, limit_search=1000, display_type="Top", resume=False, proxy=None):
 
     ''' 
     scrap data from twitter using requests, starting from start_date until max_date. The bot make a search between each start_date and end_date 
@@ -217,7 +221,7 @@ def scrap(start_date, max_date, words,to_accounts, from_accounts, days_between=5
     '''
 
     #initiate the driver
-    driver=init_driver(navig, headless)
+    driver=init_driver(navig, headless, proxy)
 
     data = []
     tweet_ids = set()
@@ -299,29 +303,31 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Scrap tweets.')
 
     parser.add_argument('--words', type=str,
-                    help='queries. they should be devided by "//" : Cat//Dog.',default = None)
+                    help='Queries. they should be devided by "//" : Cat//Dog.',default = None)
     parser.add_argument('--from_account', type=str,
                     help='Tweets from this account (axample : @Tesla).',default = None)
     parser.add_argument('--to_account', type=str,
                     help='Tweets replyed to this account (axample : @Tesla).',default = None)
     parser.add_argument('--max_date', type=str,
-                    help='max date for search query. example : %%Y-%%m-%%d.',required=True)
+                    help='Max date for search query. example : %%Y-%%m-%%d.',required=True)
     parser.add_argument('--start_date', type=str,
-                    help='start date for search query. example : %%Y-%%m-%%d.',required=True)
+                    help='Start date for search query. example : %%Y-%%m-%%d.',required=True)
     parser.add_argument('--interval', type=int,
                     help='Interval days between each start date and end date for search queries. example : 5.', default=1)
     parser.add_argument('--navig', type=str,
-                    help='navigator to use : chrome or edge.', default = "chrome")
+                    help='Navigator to use : chrome or edge.', default = "chrome")
     parser.add_argument('--lang', type=str,
-                    help='tweets language. example : "en" for english and "fr" for french.', default = None)
+                    help='Tweets language. example : "en" for english and "fr" for french.', default = None)
     parser.add_argument('--headless', type=bool,
-                    help='headless webdrives or not. True or False', default=False)
+                    help='Headless webdrives or not. True or False', default=False)
     parser.add_argument('--limit', type=int,
                     help='Limit tweets per <interval>', default=1000)
     parser.add_argument('--display_type', type=str,
                     help='Display type of twitter page : Latest or Top', default="Top")
     parser.add_argument('--resume', type=bool,
-                    help='resume the last scraping. specify the csv file path.', default=False)
+                    help='Resume the last scraping. specify the csv file path.', default=False)
+    parser.add_argument('--proxy', type=str,
+                    help='Proxy server', default=None)
 
 
     args = parser.parse_args()
@@ -338,5 +344,6 @@ if __name__ == '__main__':
     from_account = args.from_account
     to_account = args.to_account
     resume=args.resume
+    proxy = args.proxy
 
-    data=scrap(start_date, max_date, words, to_account, from_account,interval,navig,lang, headless, limit,display_type,resume)
+    data=scrap(start_date, max_date, words, to_account, from_account,interval,navig,lang, headless, limit,display_type,resume,proxy)
