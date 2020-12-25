5fe10948be0de9e1627badfa3371a18c2b27ae68
soufiane.bengadi <soufiane.bengadi@fashiondata.io>
2020-12-25 22:23:54 +0100

    pip install Scweet==0.1.2

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from distutils.core import setup
 
-with open('./requirements.txt') as requirements:
-    required = requirements.read().splitlines()
-
 setup(
   name = 'Scweet',
   packages = ['Scweet'],
-  version = '0.1',
+  version = '0.1.2',
   license='MIT',
   description = 'Tool for scraping Tweets',
   author = 'Soufiane and Yassine',
   author_email = 'bokudakgainaimachi@gmail.com',
   url = 'https://github.com/Altimis/Scweet',
-  download_url = 'https://github.com/Altimis/Scweet/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/Altimis/Scweet/archive/0.1.1.tar.gz',
   keywords = ['twitter', 'scraper', 'python', "crawl"],
-  install_requires=required,
+  install_requires=['msedge-selenium-tools', 'selenium', 'pandas'],
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
