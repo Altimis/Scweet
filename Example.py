961514d5fc67642332166f5f3ba6e1c44a433ea0
Yassine Ait Jeddi <yassine.aitjeddi@centrale-casablanca.ma>
2021-08-09 13:01:12 +0200

    Add limit to get_follow

--- Example.py
+++ Example.py
@@ -4,30 +4,29 @@ from Scweet.user import get_user_information, get_users_following, get_users_fol
 
 # scrape top tweets with the words 'covid','covid19' in proximity and without replies.
 # the process is slower as the interval is smaller (choose an interval that can divide the period of time betwee, start and max date)
-"""
+
 data = scrap(words=['bitcoin','ethereum'], since="2015-04-01", until="2015-04-15", from_account = None, interval=1, 
 	headless=True, display_type="Top", save_images=False, 
 	resume=False, filter_replies=False, proximity=False)
-"""
+
 # scrape top tweets of with the hashtag #covid19, in proximity and without replies.
 # the process is slower as the interval is smaller (choose an interval that can divide the period of time betwee, start and max date)
-"""
+
 data = scrap(hashtag="bitcoin", since="2021-08-05", until=None, from_account = None, interval=1, 
 	headless=True, display_type="Top", save_images=False, 
-<<<<<<< Updated upstream
 	resume=False, filter_replies=True, proximity=True)
-"""
+
 
 # Get the main information of a given list of users
 # These users belongs to my following. 
 
-users = ['nagouzil', '@yassineaitjeddi',] #'TahaAlamIdrissi', 
-         #'@Nabila_Gl', 'geceeekusuu', '@pabu232', '@av_ahmet', '@x_born_to_die_x']
+users = ['nagouzil', '@yassineaitjeddi',] 'TahaAlamIdrissi', 
+         '@Nabila_Gl', 'geceeekusuu', '@pabu232', '@av_ahmet', '@x_born_to_die_x']
 
 # this function return a list that contains : 
 # ["nb of following","nb of followers", "join date", "birthdate", "location", "website", "description"]
 
-#users_info = get_user_information(users, headless=True)
+users_info = get_user_information(users, headless=True)
 
 
 # Get followers and following of a given list of users
@@ -37,6 +36,6 @@ users = ['nagouzil', '@yassineaitjeddi',] #'TahaAlamIdrissi',
 # set your .env file with SCWEET_USERNAME and SCWEET_PASSWORD variables and provide its path
 env_path = ".env"
 
-following = get_users_following(users=users, env=env_path, verbose=0, headless=True, wait=3, limit=50, file_path=None)
+following = get_users_following(users=users, env=env_path, verbose=0, headless=True, wait=2, limit=50, file_path=None)
 
-followers = get_users_followers(users=users, env=env_path, verbose=0, headless=True, wait=3, limit=50, file_path=None)
+followers = get_users_followers(users=users, env=env_path, verbose=0, headless=True, wait=2, limit=50, file_path=None)
